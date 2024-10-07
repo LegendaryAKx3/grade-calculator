7001106d03b83cc3bad00439caa05755f2997641
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2024-10-07 10:26:11 -0400

    Favicon

--- grade-calculator/app/layout.tsx
+++ grade-calculator/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Grade Calculator",
   description: "Calculate your grades",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
